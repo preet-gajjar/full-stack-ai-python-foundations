52ee10b5f2dd7f72d62529ef21256baf74155b5c
Preet Gajjar <preetsgajjar3076@gmail.com>
2026-06-19 19:39:23 +0530

    Boolean and None datatype

--- 02_datatypes/chapter_4.py
+++ 02_datatypes/chapter_4.py
@@ -3,7 +3,7 @@
 is_boiling = True # boolean datatype
 a = 5
 total = is_boiling + a
-print(f"Total: {total}") # Total: 6
+print(f"Total: {total}")   # Total: 6
 
 milk_present = 0 # No milk
 print(f"Is there any milk present?: {bool(milk_present)}") # bool function
