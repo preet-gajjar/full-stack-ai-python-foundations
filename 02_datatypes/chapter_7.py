2064fbc93513044cf06b13143fc51a023e8dd9bc
Preet Gajjar <preetsgajjar3076@gmail.com>
2026-06-19 23:41:04 +0530

    Tested tuple operations

--- 02_datatypes/chapter_7.py
+++ 02_datatypes/chapter_7.py
@@ -5,9 +5,9 @@ main_spices = ("cinnamon", "cloves", "cardamom")
 
 print(f"Total spices: {main_spices}")
 
-cinnamon_ratio, cloves_ratio = 2, 1
-print(f"The ratio of Cinnamon is {cinnamon_ratio} and ratio of cloves is {cloves_ratio}")
-cinnamon_ratio, cloves_ratio = cloves_ratio, cinnamon_ratio #swapping
-print(f"The ratio of Cinnamon is {cinnamon_ratio} and ratio of cloves is {cloves_ratio}")
+cinnamon_ratio, clove_ratio = 2, 1
+print(f"The ratio of Cinnamon is {cinnamon_ratio} and ratio of cloves is {clove_ratio}")
+cinnamon_ratio, clove_ratio = clove_ratio, cinnamon_ratio #swapping
+print(f"The ratio of Cinnamon is {cinnamon_ratio} and ratio of cloves is {clove_ratio}")
 
 print(f"Is ginger available in main spices? {'ginger' in main_spices}")
