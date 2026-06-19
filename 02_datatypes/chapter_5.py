d3b8b2e5fc828b0b1a22aff6cb79eb298a32f466
Preet Gajjar <preetsgajjar3076@gmail.com>
2026-06-19 21:12:50 +0530

    Real Numbers

--- 02_datatypes/chapter_5.py
+++ 02_datatypes/chapter_5.py
@@ -7,10 +7,10 @@ ideal_temp = Decimal('95')
 current_temp = Decimal('94.99')
 long_temp = Decimal('94.999999999999')
 
-temp_diff = ideal_temp - current_temp
+temp_difference = ideal_temp - current_temp
 temp_long_diff = ideal_temp - long_temp
 
-print(f"temp_diff: {temp_diff}")
-print(f"long_temp_diff: {temp_long_diff:f}") # ':f' for formated decimal output 
+print(f"temp_difference: {temp_difference}")
+print(f"long_temp_difference: {temp_long_diff:f}") # ':f' for formated decimal output 
 
 
